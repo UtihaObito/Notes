11aecead67b2134c7b66ef168abbaf74bd740da1
ssssssssssssssss <mitia.fall2015@yandex.ru>
2021-12-03 03:13:47 +0300

    Xamarin5

--- Notes/Notes.iOS/Main.cs
+++ Notes/Notes.iOS/Main.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using Foundation;
 using UIKit;
 
@@ -12,9 +11,7 @@ namespace Notes.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
